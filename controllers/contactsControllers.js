0396a6f178361df5bf397e11041f0da91542eb18
Olena Voina <105532756+olli20@users.noreply.github.com>
2024-03-17 20:43:45 +0100

    in progress

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,8 +1,23 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const contacts = await listContacts();
+  //   console.log(contacts);
+  res.json(contacts);
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  const url = req.url;
+  const id = url.slice(1);
+  const contact = await getContactById(id);
+  //   console.log(id);
+  res.json(contact);
+};
 
 export const deleteContact = (req, res) => {};
 
---
+++ services/contactsServices.js
@@ -0,0 +1,35 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = "./db/contacts.json";
+
+export const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(contacts);
+};
+
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+};
+
+export const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+export const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
